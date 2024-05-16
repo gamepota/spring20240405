d554a76466955202f5091d9a9a6ab63d98b20bdc
Dongwon <kdw686968@gmail.com>
2024-05-16 12:04:54 +0900

    /api/someurl4 의 post 요청처리메소드

---
+++ src/main/java/com/study/controller/Controller41.java
@@ -0,0 +1,36 @@
+package com.study.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/main41")
+public class Controller41 {
+
+    @PostMapping("sub1")
+    public void sub1(@RequestBody Map<String, Object> map) {
+        // @RequestBody
+        // 역직렬화(파싱, 디코딩) JSON -> Map
+        System.out.println("map = " + map);
+    }
+
+    @PostMapping("sub2")
+    public void sub2(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
+
+    @PostMapping("sub3")
+    public void sub3(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
+
+    @PostMapping("sub4")
+    public void sub4(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
+}
