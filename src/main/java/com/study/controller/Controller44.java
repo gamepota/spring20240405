127745dafc27523e0732abeffaa05e6667f9fff1
Dongwon <kdw686968@gmail.com>
2024-05-17 12:18:02 +0900

    토큰생성시 권한 포함
    권한 별 접근 가능한 메소드 작성

--- src/main/java/com/study/controller/Controller44.java
+++ src/main/java/com/study/controller/Controller44.java
@@ -1,14 +1,12 @@
 package com.study.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.Map;
@@ -22,17 +20,54 @@ public class Controller44 {
 
     @PostMapping("login")
     @ResponseBody
-    public String login(@RequestBody Map<String, String> map) {
+    public String login(@RequestBody Map<String, Object> map) {
+        // 사용자 이름
         String username = map.get("username").toString();
-        //권한
-//        map.get("scope");
+        // 권한
+        String scope = map.get("scope").toString();
+
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(Instant.now())
                 .expiresAt(Instant.now().plusSeconds(60 * 60 * 24))
-//                .claim("scope","") 권한
+                .subject(username)
+                .claim("scope", scope) // 권한
                 .build();
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-        
     }
+
+    @GetMapping("all")
+    @ResponseBody
+    public String all() {
+        return "누구나 접근 가능한 경로";
+    }
+
+    @GetMapping("user")
+    @ResponseBody
+    @PreAuthorize("isAuthenticated()")
+    public String user() {
+        return "로그인 한 유저만 접근 가능한 경로";
+    }
+
+    @GetMapping("admin")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
+    public String admin() {
+        return "어드민만 접근 가능한 경로";
+    }
+
+    @GetMapping("manager")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('SCOPE_manager')")
+    public String manager() {
+        return "매니저만 접근 가능한 경로";
+    }
+
+    @GetMapping("ma")
+    @ResponseBody
+    @PreAuthorize("hasAnyAuthority('SCOPE_manager', 'SCOPE_admin')")
+    public String ma() {
+        return "매니져 또는 어드민이 접근 가능한 경로";
+    }
+
 }
